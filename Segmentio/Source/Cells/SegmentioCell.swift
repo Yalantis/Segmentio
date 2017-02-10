3dd643dd29602de9e74cc03e4b377a53129de0e9
Patrick <patrick.negretto@laser-group.com>
2017-02-10 19:13:44 +0100

    fix image tint color

--- Segmentio/Source/Cells/SegmentioCell.swift
+++ Segmentio/Source/Cells/SegmentioCell.swift
@@ -140,6 +140,7 @@ class SegmentioCell: UICollectionViewCell {
             segmentTitleLabel?.textColor = selected ? selectedState.titleTextColor : defaultState.titleTextColor
             segmentTitleLabel?.font = selected ? selectedState.titleFont : defaultState.titleFont
             backgroundColor = selected ? selectedState.backgroundColor : defaultState.backgroundColor
+            segmentImageView?.tintColor = selected ? selectedState.imageTintColor : defaultState.imageTintColor
         }
     }
     
--- Segmentio/Source/SegmentioOptions.swift
+++ Segmentio/Source/SegmentioOptions.swift
@@ -41,14 +41,17 @@ public struct SegmentioState {
     var backgroundColor: UIColor
     var titleFont: UIFont
     var titleTextColor: UIColor
+    var imageTintColor: UIColor
     
     public init(
         backgroundColor: UIColor = .clear,
         titleFont: UIFont = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize),
-        titleTextColor: UIColor = .black) {
+        titleTextColor: UIColor = .black,
+        imageTintColor: UIColor? = nil) {
         self.backgroundColor = backgroundColor
         self.titleFont = titleFont
         self.titleTextColor = titleTextColor
+        self.imageTintColor = imageTintColor ?? titleTextColor
     }
     
 }
