0d865116d06e047b8c866142a095a769be89e97c
Lan Chee Hong <cheehong.lan@myboost.com.my>
2022-08-11 14:51:27 +0800

    feat: add xcframework to proj

---
+++ Segmentio.xcframework/ios-arm64_armv7/Segmentio.framework/Headers/Segmentio.h
@@ -0,0 +1,19 @@
+//
+//  Segmentio.h
+//  Segmentio
+//
+//  Created by Dmitriy Demchenko on 7/11/16.
+//  Copyright © 2016 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Segmentio.
+FOUNDATION_EXPORT double SegmentioVersionNumber;
+
+//! Project version string for Segmentio.
+FOUNDATION_EXPORT const unsigned char SegmentioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Segmentio/PublicHeader.h>
+
+
